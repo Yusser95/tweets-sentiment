e8e3dca696088d5d893626c105aa466c78a57301
MAC <mac@MACs-MacBook-Pro.local>
2018-12-10 03:45:37 +0200

    V1

--- app.py
+++ app.py
@@ -188,5 +188,6 @@ def createuser():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    # app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True, threaded=True)
 
